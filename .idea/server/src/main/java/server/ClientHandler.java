c35388d5338b3101c8483eba2a8218cc894fb6c4
sanan90 <san-an@bk.ru>
2020-09-24 20:41:10 +0300

    HW

---
+++ .idea/server/src/main/java/server/ClientHandler.java
@@ -0,0 +1,146 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.sql.*;
+
+public class ClientHandler {
+
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private String  nickname;
+    private String login;
+
+
+    public ClientHandler(Server server, Socket socket) throws SocketTimeoutException {
+        try{
+            this.server = server;
+            this.socket = socket;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+
+
+            new Thread(()-> {
+                try {
+
+                    socket.setSoTimeout(120000);
+
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.equals("/end")) {
+                            out.writeUTF("/end");
+                            break;
+                        }
+
+
+                        if (str.startsWith("/auth")) {
+                            String[] token = str.split("\\s");
+                            if (token.length < 3) {
+                                continue;
+                            }
+                            String newNick = server.getAuthService()
+                                    .getNickNameByLoginAndPassword(token[1], token[2]);
+                            login = token[1];
+                            if (newNick != null) {
+                                if (!server.isLoginAuthenticated(login)) {
+                                    nickname = newNick;
+                                    sendMsg("/authok " + nickname);
+                                    server.subscribe(this);
+                                    System.out.println("Клиент " + nickname + " подключен");
+                                    socket.setSoTimeout(0);
+
+                                    break;
+                                } else {
+                                    sendMsg("Данной учеткой уже пользуются");
+                                }
+                            } else {
+                                sendMsg("Неверный логин или пароль");
+                            }
+                        }
+
+                        if (str.startsWith("/reg ")) {
+                            String[] token = str.split("\\s");
+                            if (token.length < 4) {
+                                continue;
+                            }
+                            boolean b = server.getAuthService()
+                                    .registration(token[1], token[2], token[3]);
+                            if (b) {
+                                socket.setSoTimeout(0);
+                                sendMsg("/regok");
+
+
+                            } else {
+                                sendMsg("/regno");
+                            }
+                        }
+                    }
+
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                out.writeUTF("/end");
+                                break;
+                        }
+                            if (str.startsWith("/w")) {
+                                String[] token = str.split("\\s+", 3);
+                                if (token.length<3){
+                                    continue;
+                                }
+                                server.privateMsg(this, token[1], token[2]);
+                            }
+                        }   else {
+                            server.broadcastMsg(this, str);
+                        }
+                    }
+                }   catch (SocketTimeoutException e) {
+                    socket.isClosed();
+                    sendMsg("Время вышло");
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                } finally {
+                    System.out.println("Клиент отключился");
+                    server.unsubscribe(this);
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            ).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    void sendMsg(String msg){
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
---
+++ src/src/server/src/main/java/server/Start.java
@@ -0,0 +1,12 @@
+package server;
+
+import java.sql.*;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        new Server();
+
+    }
+}
